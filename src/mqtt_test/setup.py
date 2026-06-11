3be26fea66f1ed42d6951fefc598ef4706da339e
Parichu <orga_aume@hotmail.co.th>
2026-06-11 13:57:10 +0700

    Update Setup File

--- src/mqtt_test/setup.py
+++ src/mqtt_test/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "mqtt_test"
@@ -9,12 +12,14 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="parichu",
     maintainer_email="parichu@todo.todo",
-    description="TODO: Package description",
+    description="Camera-to-map navigation with Nav2",
     license="TODO: License declaration",
     extras_require={
         "test": [
@@ -25,7 +30,9 @@ setup(
         "console_scripts": [
             "mqtt_pub = mqtt_test.pub_cmd_vel:main",
             "nav_node = mqtt_test.nav:main",
-            "mq_pub = mqtt_test.mq_pub:main",
+            "cam_nav = mqtt_test.cam_nav:main",
+            "loopback_sim = mqtt_test.loopback_sim:main",
+            "click_nav = mqtt_test.click_point:main",
         ],
     },
 )
